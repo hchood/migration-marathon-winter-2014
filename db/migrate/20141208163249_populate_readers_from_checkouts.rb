1ce62eef1fb269559fbef828e6c36ccaec4d4ee4
Helen Hood <helen.c.hood@gmail.com>
2014-12-08 11:41:58 -0500

    Modify populate readers migration

--- db/migrate/20141208163249_populate_readers_from_checkouts.rb
+++ db/migrate/20141208163249_populate_readers_from_checkouts.rb
@@ -9,7 +9,14 @@ class PopulateReadersFromCheckouts < ActiveRecord::Migration
     checkouts = Checkout.all
 
     checkouts.each do |checkout|
-      reader = Reader.find_or_create_by(full_name: checkout.reader_name, email: "N/A", phone_number: "N/A")
+      reader = Reader.find_by(full_name: checkout.reader_name)
+      if reader.nil?
+        reader = Reader.create(full_name: checkout.reader_name, email: "N/A", phone_number: "N/A")
+        puts "Reader #{reader.full_name} created."
+      else
+        puts "Reader #{reader.full_name} already in database."
+      end
+
       checkout.update_attribute(reader_id: reader.id)
       puts "Checkout #{checkout.id}'s reader, #{reader.full_name}, updated."
     end
