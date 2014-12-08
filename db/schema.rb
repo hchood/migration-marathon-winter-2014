6c9fc75651c43dd77e074196092e105e82639ecb
Helen Hood <helen.c.hood@gmail.com>
2014-12-08 11:05:05 -0500

    Drop category_id from books table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208160000) do
+ActiveRecord::Schema.define(version: 20141208160347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20141208160000) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "author"
-    t.integer  "category_id"
   end
 
   add_index "books", ["call_number"], name: "index_books_on_call_number", unique: true, using: :btree
